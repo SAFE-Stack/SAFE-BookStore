8b6e1bc37d8549d9ba9a5cf4b902c6b91ab73c62
Eugene Tolmachev <eugene.tolmachev@prolucid.ca>
2017-02-01 17:37:30 -0500

    adding appveyor pre-req

--- build.fsx
+++ build.fsx
@@ -64,12 +64,14 @@ let runWithLog log cmd args dir =
         | 0 -> ()
         | _ -> failwithf "Error while running '%s' with args: %s" cmd args
 
-let platformTool tool path =
-    isUnix |> function | true -> tool | _ -> path
+let platformTool tool winTool =
+    if isUnix then tool else winTool
+    |> ProcessHelper.tryFindFileOnPath
+    |> function Some t -> t | _ -> failwithf "%s not found" tool 
 
-let nodePath = platformTool "node" (@"C:\Program Files\nodejs\node.exe" |> FullName)
+let nodePath = platformTool "node" "node.exe"
 
-let npmTool = platformTool "npm" (@"C:\Program Files\nodejs\npm.cmd" |> FullName)
+let npmTool = platformTool "npm" "npm.cmd"
 
 let runFableWithLog log projDir args =
     runWithLog log nodePath ("node_modules/fable-compiler " + projDir + " " + args) "."
