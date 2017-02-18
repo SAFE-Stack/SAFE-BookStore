b08b0428cfb3c34b1ea01d96c9fc95a5948fac5e
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2017-02-18 11:26:34 +0100

    Store WishList as JSON file

--- src/Server/WishList.fs
+++ src/Server/WishList.fs
@@ -11,21 +11,42 @@ open Suave.RequestErrors
 open System
 open Suave.ServerErrors
 
+let defaultWishList userName : Domain.WishList =
+    {
+        UserName = userName
+        Books = 
+            [{ Title = "Mastering F#"
+               Authors = ["Alfonso Garcia-Caro Nunez"]
+               Link = "https://www.amazon.com/Mastering-F-Alfonso-Garcia-Caro-Nunez-ebook/dp/B01M112LR9" }
+             { Title = "Learn F#"
+               Authors = ["Isaac Abraham"]
+               Link = "https://www.manning.com/books/learn-fsharp" }]
+    }
+
+let getJSONFileName userName = sprintf "./temp/db/%s.json" userName
+
+let getWishListFromDB userName =
+    let fi = FileInfo(getJSONFileName userName)
+    if not fi.Exists then
+        defaultWishList userName
+    else
+        File.ReadAllText(fi.FullName)
+        |> JsonConvert.DeserializeObject<Domain.WishList>
+
+let saveWishListToDB (wishList:Domain.WishList) =
+    try
+        let fi = FileInfo(getJSONFileName wishList.UserName)
+        if not fi.Directory.Exists then
+            fi.Directory.Create()
+        File.WriteAllText(fi.FullName,JsonConvert.SerializeObject wishList)
+    with
+    | _ -> ()
+
+
 let getWishList (ctx: HttpContext) =
     Auth.useToken ctx (fun token -> async {
         try
-            // TODO: Get from DB
-            let wishList : Domain.WishList =
-                {
-                    UserName = token.UserName
-                    Books = 
-                        [{ Title = "Mastering F#"
-                           Authors = ["Alfonso Garcia-Caro Nunez"]
-                           Link = "https://www.amazon.com/Mastering-F-Alfonso-Garcia-Caro-Nunez-ebook/dp/B01M112LR9" }
-                         { Title = "Learn F#"
-                           Authors = ["Isaac Abraham"]
-                           Link = "https://www.manning.com/books/learn-fsharp" }]
-                }
+            let wishList = getWishListFromDB token.UserName
             return! Successful.OK (JsonConvert.SerializeObject wishList) ctx
         with
         | _ -> return! SERVICE_UNAVAILABLE "Database not available" ctx
@@ -38,9 +59,12 @@ let postWishList (ctx: HttpContext) =
                 ctx.request.rawForm
                 |> System.Text.Encoding.UTF8.GetString
                 |> JsonConvert.DeserializeObject<Domain.WishList>
-
-                // TODO: Save to DB
-            return! Successful.OK (JsonConvert.SerializeObject wishList) ctx
+            
+            if token.UserName <> wishList.UserName then
+                return! UNAUTHORIZED (sprintf "WishList is not matching user %s" token.UserName) ctx
+            else
+                saveWishListToDB wishList
+                return! Successful.OK (JsonConvert.SerializeObject wishList) ctx
         with
         | _ -> return! SERVICE_UNAVAILABLE "Database not available" ctx
     })    
