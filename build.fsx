26419fbda27f0e76693f7e4a3d5f8f2e2d564a61
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2017-02-21 13:25:24 +0100

    Push with tag from release notes

--- build.fsx
+++ build.fsx
@@ -336,7 +336,7 @@ Target "Deploy" (fun _ ->
         ExecProcess (fun info ->
             info.FileName <- "docker"
             info.WorkingDirectory <- deployDir
-            info.Arguments <- sprintf "push %s/%s" dockerUser dockerImageName) TimeSpan.MaxValue
+            info.Arguments <- sprintf "push %s/%s:%s" dockerUser dockerImageName release.NugetVersion) TimeSpan.MaxValue
     if result <> 0 then failwith "Docker push tag failed"    
 )
 
