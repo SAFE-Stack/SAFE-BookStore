be45785f8d799af3fd6a0b2fff50a97929b7f395
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2017-02-22 13:17:12 +0100

    Don't use live reporter

--- test/UITests/Runner.fs
+++ test/UITests/Runner.fs
@@ -4,7 +4,6 @@ open System.IO
 open canopy
 open Expecto
 open System.Diagnostics
-open reporters
 open System
 
 let rec findPackages (di:DirectoryInfo) =
@@ -19,8 +18,6 @@ let executingDir () = System.IO.Path.GetDirectoryName(System.Reflection.Assembly
 
 let startChrome() = 
     canopy.configuration.chromeDir <- executingDir()
-    reporter <- new LiveHtmlReporter(Chrome, configuration.chromeDir) :> IReporter
-    reporter.setEnvironment "canopy test page"
     start chrome
 
 [<EntryPoint>]
