85d6207dc6cade8600a4ea60a17c0083b341bb3d
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2017-05-20 23:44:03 +0200

    New try to fix Travis

--- build.fsx
+++ build.fsx
@@ -102,6 +102,7 @@ Target "InstallClient" (fun _ ->
     printfn "Node version:"
     run nodeTool "--version" __SOURCE_DIRECTORY__
     printfn "Yarn version:"
+    run yarnTool "--version" __SOURCE_DIRECTORY__
     run yarnTool "install" __SOURCE_DIRECTORY__
     runDotnet clientPath "restore"
 )
