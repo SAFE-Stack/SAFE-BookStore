534bf37c86bca8d0399f8de9bb716e8ba35d7875
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2017-09-07 14:10:48 +0200

    Resolve Babel options

--- src/Client/webpack.config.js
+++ src/Client/webpack.config.js
@@ -1,14 +1,15 @@
 var path = require("path");
 var webpack = require("webpack");
+var fableUtils = require("fable-utils");
 
 function resolve(filePath) {
   return path.join(__dirname, filePath)
 }
 
-var babelOptions = {
+var babelOptions = fableUtils.resolveBabelOptions({
   presets: [["es2015", {"modules": false}]],
   plugins: ["transform-runtime"]
-}
+});
 
 var isProduction = process.argv.indexOf("-p") >= 0;
 var port = process.env.SUAVE_FABLE_PORT || "8085";
@@ -21,6 +22,9 @@ module.exports = {
     path: resolve('./public'),
     publicPath: "/public",
     filename: "bundle.js"
+  },
+  resolve: {
+    modules: [ resolve("../../node_modules/")]
   },
   devServer: {
     proxy: {
@@ -50,11 +54,6 @@ module.exports = {
           options: babelOptions
         },
       }
-    ]
-  },
-  resolve: {
-    modules: [
-      "node_modules", resolve("./node_modules/")
     ]
   }
 };
