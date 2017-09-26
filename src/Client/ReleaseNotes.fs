5c30e85cba3672a6181dba75c73367d12b7d47fb
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2017-09-26 10:17:34 +0200

    Bumping version to 0.4.0

--- src/Client/ReleaseNotes.fs
+++ src/Client/ReleaseNotes.fs
@@ -1,11 +1,11 @@
 module internal ReleaseNotes
 
-let Version = "0.3.4"
+let Version = "0.4.0"
 
 let IsPrerelease = false
 
 let Notes = """
-### 0.3.4 - 2017-09-26
+### 0.4.0 - 2017-09-26
 * Using .NET Core 2.0
 
 ### 0.1.8 - 2017-06-17
