104db95e839443a417c277e2279729117d2f80e2
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2017-09-26 10:10:54 +0200

    Bumping version to 0.3.3

--- src/Client/ReleaseNotes.fs
+++ src/Client/ReleaseNotes.fs
@@ -1,13 +1,13 @@
 module internal ReleaseNotes
 
-let Version = "0.3.2"
+let Version = "0.3.3"
 
 let IsPrerelease = false
 
 let Notes = """
-### 0.3.2 - 25.09.2017
+### 0.3.3 - 2017-09-26
 * Using .NET Core 2.0
 
-### 0.1.8 - 17.06.2017
+### 0.1.8 - 2017-06-17
 * First Azure app services version under http://fable-suave.azurewebsites.net
 """
