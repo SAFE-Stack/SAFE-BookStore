78fa5f6e1d498a898ab12794e7f42d942fc82a47
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2017-09-29 14:58:56 +0200

    use dotnet-fable beta

--- src/Client/webpack.config.js
+++ src/Client/webpack.config.js
@@ -44,7 +44,8 @@ module.exports = {
           loader: "fable-loader",
           options: {
             babel: babelOptions,
-            define: isProduction ? [] : ["DEBUG"]
+            define: isProduction ? [] : ["DEBUG"],
+            extra: { optimizeWatch: true }
           }
         }
       },
