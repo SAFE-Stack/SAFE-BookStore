420696209d57d59564eca5b11509b9024a182760
Isaac Abraham <isaac.abraham@gmail.com>
2017-10-04 12:40:43 +0200

    Fix build

--- test/ServerTests/Tests.fs
+++ test/ServerTests/Tests.fs
@@ -6,7 +6,7 @@ open ServerCode
 let wishListTests =
   testList "Wishlist" [
     testCase "default contains F# mastering book" <| fun _ ->
-      let defaults =  WishList.defaultWishList "test"
+      let defaults =  ServerCode.Storage.FileSystem.defaultWishList "test"
       Expect.isNonEmpty defaults.Books "Default Books list should have at least one item"
       Expect.isTrue
         (defaults.Books |> Seq.exists (fun b -> b.Title = "Mastering F#")) 
