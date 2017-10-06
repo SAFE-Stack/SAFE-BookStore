41ad1d84324cc390e2242528f80ca704868f8d25
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2017-10-06 14:54:34 +0200

    Bumping version to 0.6.0

--- src/Client/ReleaseNotes.fs
+++ src/Client/ReleaseNotes.fs
@@ -1,10 +1,13 @@
 module internal ReleaseNotes
 
-let Version = "0.5.4"
+let Version = "0.6.0"
 
 let IsPrerelease = false
 
 let Notes = """
+### 0.6.0 - 2017-10-06
+* Azure storage
+
 ### 0.5.4 - 2017-09-26
 * Automatic deploy and push to azure
 
