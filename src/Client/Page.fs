74d2caca1a2d8cc0a23d659c37307bf7573fae48
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2017-10-11 15:19:28 +0200

    Indent

--- src/Client/Page.fs
+++ src/Client/Page.fs
@@ -1,14 +1,13 @@
 module Client.Pages
 
-
 open Elmish.Browser.UrlParser
 
 /// The different pages of the application. If you add a new page, then add an entry here.
 [<RequireQualifiedAccess>]
 type Page = 
-| Home 
-| Login
-| WishList
+    | Home
+    | Login
+    | WishList
 
 let toHash =
     function
