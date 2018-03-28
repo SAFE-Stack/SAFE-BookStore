91c970a84de630bbd29ef235d98c941d7b059786
Steffen Forkmann <sforkmann@gmail.com>
2018-03-28 08:11:25 +0200

    Put scripts into head

--- src/Server/Templates.fs
+++ src/Server/Templates.fs
@@ -26,21 +26,23 @@ let index (model: Model option) =
     | None ->
         "null", ""
   html []
-    [ head [] [ meta [ _httpEquiv "Content-Type"; _content "text/html"; _charset "utf-8" ] ]
-      title [] [ rawText "SAFE-Stack sample" ]
-      link
-        [ _rel "stylesheet"
-          _href "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css";
-          attr "integrity" "sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
-          _crossorigin "anonymous"
-        ]
-      link
-        [ _rel "stylesheet"
-          _href "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
-          attr "integrity" "sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp"
-          _crossorigin "anonymous" ]
-      link [ _rel "stylesheet"; _href "css/site.css" ]
-      link [ _rel "shortcut icon"; _type "image/png"; _href "/Images/safe_favicon.png" ]
+    [ head [] [ 
+        meta [ _httpEquiv "Content-Type"; _content "text/html"; _charset "utf-8" ]
+        title [] [ rawText "SAFE-Stack sample" ]
+        link
+          [ _rel "stylesheet"
+            _href "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css";
+            attr "integrity" "sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+            _crossorigin "anonymous"
+          ]
+        link
+          [ _rel "stylesheet"
+            _href "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+            attr "integrity" "sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp"
+            _crossorigin "anonymous" ]
+        link [ _rel "stylesheet"; _href "css/site.css" ]
+        link [ _rel "shortcut icon"; _type "image/png"; _href "/Images/safe_favicon.png" ]
+      ]
       body [ _class "app-container" ] [
         div [ _id "elmish-app"; _class "elmish-app" ] [
           rawText htmlStr
