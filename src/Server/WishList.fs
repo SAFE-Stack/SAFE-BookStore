35d132f9b53de1c1252e14ffff1325d8ddccc660
nojaf <florian.verdonck@outlook.com>
2018-05-31 22:53:11 +0200

    Added post wishlist

--- src/Server/WishList.fs
+++ src/Server/WishList.fs
@@ -10,6 +10,11 @@ open Freya.Machines
 open ServerCode.Database
 open Freya.Machines.Http
 open Freya.Types.Http
+open Server
+open Freya.Optics.Http
+open Server.Represent
+open Freya.Optics.Http.Cors
+open Freya.Machines.Http
 
 let getUser = 
     freya {
@@ -32,11 +37,87 @@ let getWishlist (db:IDatabaseFunctions) =
         return Server.Represent.json wishlist
     }
 
+let wishLisPostModel =
+    freya {
+        return! readJson<Domain.WishList>
+    } |> Freya.memo
+
+let isAllowedPostWishListRequest = 
+    freya {
+        let! wishList = wishLisPostModel
+        let! token = Auth.getUserFromAuthToken
+        let sameUser =
+            token
+            |> Option.map (fun t -> (t.UserName = wishList.UserName))
+            |> Option.defaultValue false
+        return sameUser
+    }
+
+let forbiddenPostList =
+    freya {
+        let! token = Auth.getUserFromAuthToken
+        let content =
+            token
+            |> Option.map (fun t ->
+                sprintf "WishList is not matching user %s" t.UserName
+            )
+            |> Option.defaultValue "WishList not ok"
+
+        return Represent.text content
+    }  
+
+let whenPost altValue fn =
+    freya {
+        let! verb = Freya.Optic.get Request.method_
+        let! res =
+            match verb = POST with
+            | true -> fn
+            | false -> Freya.init altValue
+        return res
+    }
+
+
+let isPostWishlistValid =
+    freya {
+        let! model = wishLisPostModel
+        return not(Validation.verifyWishList model)
+    }
+
+let badPostWishlist =
+    freya {
+        return Represent.text "WishList is not valid"
+    }
+
+let postWishlist (saveWishListToDB: WishList -> Task<unit>) =
+    freya {
+        let! wishList = wishLisPostModel
+        let saveTask =
+            saveWishListToDB wishList
+            |> Async.AwaitTask
+            |> Freya.fromAsync
+        do! saveTask
+    }    
+
+let wishListCreated = 
+    freya {
+        let! wishList = wishLisPostModel
+        return Represent.json wishList
+    }
+
 let wishListMachine (db:Database.IDatabaseFunctions) =
     freyaMachine {
         methods [GET;HEAD;OPTIONS;POST]
         including Auth.authMachine
         handleOk (getWishlist db)
+        
+        allowed (whenPost true isAllowedPostWishListRequest)
+        handleForbidden forbiddenPostList
+
+        badRequest (whenPost false isPostWishlistValid)
+        handleBadRequest badPostWishlist
+
+        doPost (postWishlist db.SaveWishList)
+        handleCreated wishListCreated
     }
 
 //let getWishList (getWishListFromDB : string -> Task<WishList>) (token : UserRights) : HttpHandler =
@@ -68,7 +149,7 @@ let wishListMachine (db:Database.IDatabaseFunctions) =
 //                | false -> return! forbiddenWishList token.UserName next ctx
 //            | false     -> return! invalidWishList next ctx
 //        }
-//
+
 ///// Retrieve the last time the wish list was reset.
 //let getResetTime (getLastResetTime: unit -> Task<System.DateTime>) : HttpHandler =
 //    fun next ctx ->
