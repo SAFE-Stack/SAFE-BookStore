4474ee8ca6c9d03508cc2a9cbed623c89a8e8156
Lukas Rieger <lr@live.de>
2018-08-23 22:03:56 +0200

    show how to integrate tsx (typescript) files into the build

--- src/Client/webpack.config.js
+++ src/Client/webpack.config.js
@@ -58,6 +58,10 @@ module.exports = {
             define: isProduction ? [] : ["DEBUG"]
           }
         }
+      },      
+      {
+          test: /\.tsx?$/,
+          loader: 'awesome-typescript-loader'
       },
       {
         test: /\.js$/,
