d5276a4631caf6ac4460a2f703494c6f0f8dcef8
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2018-11-29 15:26:22 +0100

    Try to restore manually

--- build.fsx
+++ build.fsx
@@ -126,6 +126,7 @@ Target "NPMInstall" (fun _ ->
 )
 
 Target "BuildClient" (fun _ ->
+    runDotnet clientPath "restore"
     run yarnTool "webpack --config src/Client/webpack.config.js -p" clientPath 
 )
 
