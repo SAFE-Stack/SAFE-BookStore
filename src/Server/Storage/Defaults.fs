197fdf7ac2b2cb93482e136333c4e09e901882b7
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2019-08-21 11:14:53 +0200

    Add yet another book

--- src/Server/Storage/Defaults.fs
+++ src/Server/Storage/Defaults.fs
@@ -11,4 +11,8 @@ let defaultWishList userName =
             Link = "https://www.amazon.com/Mastering-F-Alfonso-Garcia-Caro-Nunez-ebook/dp/B01M112LR9" }
           { Title = "Get Programming with F#"
             Authors = "Isaac Abraham"
-            Link = "https://www.manning.com/books/get-programming-with-f-sharp" } ] }
+            Link = "https://www.manning.com/books/get-programming-with-f-sharp" }
+          { Title = "Stylish F#"
+            Authors = "Kit Eason"
+            Link = "https://www.apress.com/la/book/9781484239995" } ]
+    }
