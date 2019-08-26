352c4139e8fab85fc2cf87429955c6a1a974ccf6
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2019-08-26 14:24:30 +0200

    Bumping version to 0.13.31

--- src/Client/ReleaseNotes.fs
+++ src/Client/ReleaseNotes.fs
@@ -1,11 +1,11 @@
 module internal ReleaseNotes
 
-let Version = "0.13.30"
+let Version = "0.13.31"
 
 let IsPrerelease = false
 
 let Notes = """
-### 0.13.30 - 2019-08-26
+### 0.13.31 - 2019-08-26
 * Show RenderSource
 * Updates to SSR
 
--- src/Client/views/Menu.fs
+++ src/Client/views/Menu.fs
@@ -23,7 +23,9 @@ let view onLogout (model:Model) =
 
         printfn "Rendered on server %b" model.RenderedOnServer
         if model.RenderedOnServer then
+            yield str (sprintf "Rendered on 1 server %b" model.RenderedOnServer)
             yield img [ Key "RenderedOnServer"; Src "/Images/baseline_check_box_outline_blank_black_24dp.png"; Title "Rendered on server";  ]
         else
-            yield img [ Key "RenderedOnClient"; Src "/Images/baseline_check_box_black_24dp.png"; Title "Rendered in browser" ]
+            yield str (sprintf "Rendered on 2 server %b" model.RenderedOnServer)
+            yield img [ Key "RenderedOnServer"; Src "/Images/baseline_check_box_black_24dp.png"; Title "Rendered in browser" ]
     ]
