34f93d617499f9048f67ced0917b7d46c0b5f1cb
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2019-08-26 09:36:59 +0200

    Bumping version to 0.13.10

--- src/Client/ReleaseNotes.fs
+++ src/Client/ReleaseNotes.fs
@@ -1,11 +1,11 @@
 module internal ReleaseNotes
 
-let Version = "0.13.9"
+let Version = "0.13.10"
 
 let IsPrerelease = false
 
 let Notes = """
-### 0.13.9 - 2019-08-26
+### 0.13.10 - 2019-08-26
 * Show RenderSource
 * Updates to SSR
 
