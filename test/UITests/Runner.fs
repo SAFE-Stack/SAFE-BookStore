06c52a693c82ad4d78624a19aacd1cb4e1613517
John Wostenberg <jwosty@users.noreply.github.com>
2023-03-03 19:10:58 -0700

    Fix package pinning mistake

--- test/UITests/Runner.fs
+++ test/UITests/Runner.fs
@@ -8,6 +8,7 @@ open Expecto
 let startBrowser() =
     //start Chrome // Use this if you want to see your tests in the browser
     start ChromeHeadless
+    
     resize (1280, 960)
 
 [<EntryPoint>]
