66d7a95398bd213c32df09736986de18b5436518
Martin Bryant <mail@martinbryant.dev>
2024-01-22 12:01:32 +0000

    skeleton base page/daisy ui added

---
+++ update/src/Client/Pages/Home.fs
@@ -0,0 +1,32 @@
+module Page.Home
+
+open Elmish
+
+type Page =
+    | Home
+    | Login
+    | Wishlist
+
+type Model = { Page: Page }
+
+type Msg =
+    | NoOp
+
+// let todosApi =
+//     Remoting.createApi ()
+//     |> Remoting.withRouteBuilder Route.builder
+//     |> Remoting.buildProxy<ITodosApi>
+
+let init () =
+    let model = { Page = Home }
+    model, Cmd.none
+
+let update msg model =
+    match msg with
+    | NoOp -> model, Cmd.none
+
+open Feliz
+
+let view model dispatch =
+    Html.div [ prop.text "Home" ]
+
--- update/src/Client/tailwind.config.js
+++ update/src/Client/tailwind.config.js
@@ -8,5 +8,8 @@ module.exports = {
     theme: {
         extend: {},
     },
-    plugins: []
+    plugins: [require("daisyui")],
+    daisyui: {
+        themes: ["winter"]
+    }
 }
