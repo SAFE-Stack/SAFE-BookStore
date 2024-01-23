6206847d1f6e44bb8c33006a4bf0f546c88b7649
Martin Bryant <mail@martinbryant.dev>
2024-01-23 15:40:11 +0000

    wishlist view and initial data

--- update/src/Client/Index.fs
+++ update/src/Client/Index.fs
@@ -11,14 +11,21 @@ open Shared
 type PageTab =
     | Home of Home.Model
     | Login of Login.Model
+    | Wishlist of Wishlist.Model
     | NotFound
 
-type Model = { Page: PageTab }
+type User =
+    | Guest
+    | User of UserData
+
+type Model = { Page: PageTab; User: User }
 
 type Msg =
     | HomePageMsg of Home.Msg
     | LoginPageMsg of Login.Msg
+    | WishlistMsg of Wishlist.Msg
     | UrlChanged of string list
+    | Logout
 
 let booksApi =
     Remoting.createApi ()
@@ -30,35 +37,49 @@ let userApi =
     |> Remoting.withRouteBuilder Route.builder
     |> Remoting.buildProxy<IUserApi>
 
-let initFromUrl url =
+let initFromUrl model url =
     match url with
     | [] ->
         let homeModel, homeMsg = Home.init booksApi
-        let model = { Page = Home homeModel }
+        let model = { Page = Home homeModel; User = model.User }
         let cmd = homeMsg |> Cmd.map HomePageMsg
         model, cmd
     | [ "login" ] ->
         let loginModel, loginMsg = Login.init ()
-        let model = { Page = Login loginModel }
+        let model = { Page = Login loginModel; User = model.User }
         let cmd = loginMsg |> Cmd.map LoginPageMsg
         model, cmd
-    | _ -> { Page = NotFound }, Cmd.none
+    | [ "wishlist" ] ->
+        let wishlistModel, wishlistMsg = Wishlist.init booksApi
+        let model = { Page = Wishlist wishlistModel; User = model.User }
+        let cmd = wishlistMsg |> Cmd.map WishlistMsg
+        model, cmd
+    | _ -> { Page = NotFound; User = model.User }, Cmd.none
 
 let init () =
+    let model, _ = Home.init booksApi
     Router.currentUrl ()
-    |> initFromUrl
+    |> initFromUrl { Page = Home model; User = Guest }
 
 let update msg model =
     match model.Page, msg with
     | Home homeModel, HomePageMsg homeMsg ->
         let newModel, cmd = Home.update homeMsg homeModel
-        { Page = Home newModel }, cmd
+        { Page = Home newModel; User = model.User }, cmd
     | Login loginModel, LoginPageMsg loginMsg ->
+        let user =
+            match loginMsg with
+            | Login.LoggedIn user -> User user
+            | _ -> model.User
         let newModel, cmd = Login.update userApi loginMsg loginModel
-        { Page = Login newModel }, cmd |> Cmd.map LoginPageMsg
+        { Page = Login newModel; User = user }, cmd |> Cmd.map LoginPageMsg
+    | Wishlist wishlistModel, WishlistMsg wishlistMsg ->
+        let newModel, cmd = Wishlist.update wishlistMsg wishlistModel
+        { Page = Wishlist newModel; User = model.User }, cmd
     | NotFound, _ ->
-        { Page = NotFound }, Cmd.none
-    | _, UrlChanged url -> initFromUrl url
+        { Page = NotFound; User = model.User }, Cmd.none
+    | _, UrlChanged url -> initFromUrl model url
+    | _, Logout -> { model with User = Guest }, Cmd.ofMsg (UrlChanged [])
     | _, _ ->
         model, Cmd.none
 
@@ -79,10 +100,15 @@ let navigation model dispatch =
                 prop.className "justify-self-center"
                 prop.children [
                     Daisy.tab [ prop.text "Home"; prop.onClick (fun _ -> Router.navigate "") ]
-                    Daisy.tab [ prop.text "Login"; prop.onClick (fun _ -> Router.navigate "login")]
+                    match model.User with
+                    | Guest ->
+                        Daisy.tab [ prop.text "Login"; prop.onClick (fun _ -> Router.navigate "login")]
+                    | User user ->
+                        Daisy.tab [ prop.text "Wishlist"; prop.onClick (fun _ -> Router.navigate "wishlist")]
+                        Daisy.tab [ prop.text "Logout"; prop.onClick (fun _ -> dispatch Logout)]
+                        Daisy.tab [ prop.text $"Logged in as {user.UserName}" ]
                 ]
             ]
-
         ]
     ]
 
@@ -104,6 +130,8 @@ let view model dispatch =
                                 Home.view homeModel (HomePageMsg >> dispatch)
                             | Login loginModel ->
                                 Login.view loginModel (LoginPageMsg >> dispatch)
+                            | Wishlist wishlistModel ->
+                                Wishlist.view wishlistModel (WishlistMsg >> dispatch)
                             | NotFound -> Html.div [ prop.text "Not Found" ]
                         ]
                     ]
---
+++ update/src/Client/pages/Wishlist.fs
@@ -0,0 +1,45 @@
+module Page.Wishlist
+
+open Elmish
+open Fable.Core
+open Feliz.DaisyUI
+open Shared
+
+type Model = { Wishlist: Book seq }
+
+type Msg =
+    | GotWishlist of Book seq
+
+let init (booksApi: IBooksApi) =
+    let model = { Wishlist = Seq.empty }
+    let cmd = Cmd.OfAsync.perform booksApi.getWishlist () GotWishlist
+    model, cmd
+
+let update msg model =
+    match msg with
+    | GotWishlist books ->
+        JS.console.log books
+        { Wishlist = books }, Cmd.none
+
+open Feliz
+
+let bookRow book =
+    let link = Daisy.link [ link.hover; link.primary; prop.target "_blank"; prop.text book.Title; prop.href book.Link ]
+    let image = Html.img [ prop.src book.ImageLink ]
+    Html.tr [ prop.className "hover:bg-accent"; prop.children [ Html.td link; Html.td book.Authors; Html.td image ] ]
+
+let view model dispatch =
+    Html.div [
+        prop.className "overflow-y-auto"
+        prop.children [
+            Daisy.table [
+                prop.children [
+                    Html.tbody [
+                        for book in model.Wishlist do
+                            bookRow book
+                    ]
+                    Html.thead [ Html.tr [ Html.th "Title"; Html.th "Authors"; Html.th "Image" ] ]
+                ]
+            ]
+        ]
+    ]
